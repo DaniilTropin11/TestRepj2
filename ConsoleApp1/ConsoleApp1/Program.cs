404d06d29c7ac791bf9c024e6f7e0e0312ceed12
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-09-24 10:07:04 +0700

    определение високосного года

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            string year1 = Console.ReadLine();
+
+            //string year1 = Console.ReadLine();
+
+            int number = int.Parse(year1);
+            int number2 = number % 4;
+         
+
+            if (number2 != 0)
+            {
+                Console.WriteLine($" {year1}  Не високосный год");
+            }
+            else
+                Console.WriteLine($" {year1}  Високосный год");
+
+
+
+      
+
+            Console.ReadKey();
+        }
+    }
+}
