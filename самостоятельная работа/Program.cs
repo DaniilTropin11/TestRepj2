64e1636ceb68881451ccde5853faa47845041ee1
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-10-01 08:37:33 +0700

    Create Program.cs

---
+++ самостоятельная работа/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace nasledclass
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void PrintName()
+        {
+            Console.WriteLine($"Мое имя {FirstName}{LastName}!");
+        }
+
+
+    }
+    class Student : Person
+    {
+        public void Learn()
+        {
+            PrintName();
+            Console.WriteLine(" я учусь!");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student person = new Student { FirstName = "Daniil", LastName = "Tropin" };
+            person.Learn();
+
+
+
+        }
+    }
+}
