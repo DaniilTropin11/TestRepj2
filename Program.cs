9840ee8a7679ba3ad6af87b3e51650d92d0ed3cf
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-10-06 19:07:19 +0700

    2 лаба задание
    Что-то попробовал написать дома, черновик

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        static void Main(string[] args) { }
+
+
+        private string name; 
+        private string surname;
+        private int year;
+        
+
+        public Person(string name, string surname, int year)
+        {
+            this.name = name;
+            this.surname = surname;
+            if (year < 0)
+                Console.WriteLine("вфыывфвфы");
+            else
+                this.year = year;
+
+        }
+        public Person()
+        {
+            name = "";
+            surname = "";
+            year = 0;
+        }
+
+        class Person1 : Person
+        {
+            public string ToFullString(string name, string surname, int year)
+            {
+                return name + surname + year;
+               
+            }
+            public string ToShortString(string name, string surname)
+            {
+                return name + surname;
+            }
+        }
+
+            
+        
+     }
+
+            
+
+
+
+}
+    
+        
+
