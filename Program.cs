041aaaef40dfbef8a28bc18af213f59ac543dfd9
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-10-08 09:10:32 +0700

    получилось вывести ToFullString

--- Program.cs
+++ Program.cs
@@ -1,56 +1,106 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace ConsoleApp1
+namespace ConsoleApplication4
 {
-    class Person
+    class Program
     {
-        static void Main(string[] args) { }
-
-
-        private string name; 
-        private string surname;
-        private int year;
-        
-
-        public Person(string name, string surname, int year)
+        static void Main(string[] args)
         {
-            this.name = name;
-            this.surname = surname;
-            if (year < 0)
-                Console.WriteLine("вфыывфвфы");
-            else
-                this.year = year;
 
-        }
-        public Person()
-        {
-            name = "";
-            surname = "";
-            year = 0;
+            Person std = new Person("Даниил", "Тропин", new DateTime(2001, 12, 22));
+            Console.WriteLine(std.ToFullString());
+            //Person std2 = new Person("Даниил", "Тропин", DateTime.Parse("December 22, 2001"));
+            //Console.WriteLine(std.ToString(std.BDate.ToString("dd.MM")));
         }
 
-        class Person1 : Person
+        class Person
         {
-            public string ToFullString(string name, string surname, int year)
+            private
+
+            string Name;
+            string LastName;
+            System.DateTime BDate;
+
+
+
+            public Person(string studentName, string studentLastName, DateTime studentBDate)
             {
-                return name + surname + year;
-               
+                Name = studentName;
+                LastName = studentLastName;
+                BDate = studentBDate;
             }
-            public string ToShortString(string name, string surname)
+
+
+            public Person() : this("Default_Name", "Default_Sname", new DateTime(2001, 22, 12))
+            { }
+
+
+
+            string StdName
             {
-                return name + surname;
+                get
+                {
+                    return Name;
+                }
+
             }
-        }
 
-            
-        
-     }
+            string StdLastName
+            {
+                get
+                {
+                    return LastName;
+                }
 
-            
+            }
+
+            DateTime StdBDate
+            {
+                get
+                {
+                    return BDate;
+                }
+
+            }
+
+            int intStdBdate
+            {
+                get
+                {
+                    return Convert.ToInt32(BDate);
+                }
+                set
+                {
+                    BDate = Convert.ToDateTime(value);
+                }
+
+            }
 
 
 
+           
+            public string ToFullString()
+            {
+
+                // return "имя " + Name + " Фамилия " +LastName;
+                return $"имя {Name} Фамилия {LastName} Дата рождения {StdBDate:dd MMMM}";
+                //return string.Format("имя {0} Фамилия {1} {0}",Name,LastName);
+
+
+            }
+        }               
+
+
+
+            //public string ToShortString()
+            //{
+            //    return "\n" + "Имя: " + Name + "\n" + "Фамилия: " + LastName;
+            //}
+    }
 }
-    
-        
-
