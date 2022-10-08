6494d8779ac54cb3ce473af29b236f844d966464
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-10-08 11:50:01 +0700

    доделать magazine

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -9,43 +8,85 @@ using System.Xml.Linq;
 
 namespace ConsoleApp3
 {
+    #region название региона
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Article a = new Article();
+
+            Console.WriteLine(a.ToFullString());
+            Author author = new Author("","",new DateTime(2010,10,10), "",1,"222");
+            Console.WriteLine(author.ToFullString());
+        }
+    }
+
+    #endregion
+    enum Frequency { Weekly, Monthly, Yearly }
+
+
+    public class Author
+    {
+
+
+        public Author(string name, string surName, DateTime birthDate, string location, int amountPublication, string lastName)
+        {
+            Name = name;
+            SurName = surName;
+            BirthDate = birthDate;
+            Location = location;
+            AmountPublication = amountPublication;
+            LastName = lastName;
+        }
+
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Location { get; set; }
+        public int AmountPublication { get; set; }
+        public string LastName { get; set; }
+        public string ToFullString()
+        {
+            return $"/n имя Автора{Name} /n Фамилия Автора{SurName} /n Дата рождение {BirthDate} /n Место рождение {Location} /n {AmountPublication} /n {LastName}";
+        }
+
+    }
+    class Article
+    {
+        public Article(string name, double rating, Author author)
+        {
+            Name = name;
+            Rating = rating;
+            Author = author;
+        }
+
+        public Article()
+        {
+            Name = "Какой-то артикель";
+            Rating = 3.5;
+            Author = new Author("name", "surname", new DateTime(2000, 12, 1), "location", 2007, "lastName");
+
+        }
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public Author Author { get; set; }
+
+        public string ToFullString()
+        {
+            return $"имя Артикля {Name} Рейтинг {Rating} Автор {Author}";
         }
     }
 }
-
-enum Frequency { Weekly, Monthly, Yearly }
-
-
-public class Person
-{
-    private string _name;
-    private string _surname;
-    public Person(string name, string surname)
-    {
-        _name = name;
-        _surname = surname;
        
-    }
-}
-class Article
-{
-    public Article(string name, double rating, Person author)
-    {
-        Name = name;
-        Rating = rating;
-        Author = author;
-    }
 
-    public Article()
-    {
-        Name = "Какой-то артикель";
-    }
-    public string Name { get; set; }
-    public double Rating { get; set; }
-    public Person Author { get; set; }
-}
+            
+
+
+
+
+
+
+
+
+
+
