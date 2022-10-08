d27c728159361e82a14a4581b211282468c22299
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-10-08 09:57:33 +0700

    начало второй части лабы

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+enum Frequency { Weekly, Monthly, Yearly }
+
+
+public class Person
+{
+    private string _name;
+    private string _surname;
+    public Person(string name, string surname)
+    {
+        _name = name;
+        _surname = surname;
+       
+    }
+}
+class Article
+{
+    public Article(string name, double rating, Person author)
+    {
+        Name = name;
+        Rating = rating;
+        Author = author;
+    }
+
+    public Article()
+    {
+        Name = "Какой-то артикель";
+    }
+    public string Name { get; set; }
+    public double Rating { get; set; }
+    public Person Author { get; set; }
+}
