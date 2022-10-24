970bbd1112b2bdde2025cdd60ef7bc20666d4f18
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-10-24 22:09:14 +0700

    самостоятельная работа "Игра кости"

---
+++ gamekosti/gamekosti/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class gamekosti 
+{
+    static void Main(string[] args)
+    {
+        Random dice = new Random();
+        int rol1 = dice.Next(1, 7);
+        int rol2 = dice.Next(1, 7);
+        int rol3 = dice.Next(1, 7);
+        int total = rol1 + rol2 + rol3;
+        Console.WriteLine($"Сумма трех костей= {rol1} + {rol2} + {rol3} = {total}");
+
+        if ((rol1 == rol2) || (rol2 == rol3) || (rol1 == rol3))
+        {
+            Console.WriteLine("Попадание + 2 бонусных очка ! ");
+            total += 2;
+        }
+
+        if ((rol1 == rol2) && (rol2 == rol3))
+        {
+            Console.WriteLine("КРУПНОЙ ПОПАДАНИЕ + 6 БОНУСНЫХ ОЧКОВ!");
+            total += 6;
+        }
+
+        if (total < 15)
+        {
+            Console.WriteLine("ПРОИГРЫШ!");
+        }
+        if (total >= 15) 
+        {
+            Console.WriteLine("ВЫИГРЫШ!");
+        }
+    }    
+}
