6382c8823d132cee52f172fa578a943db87280a4
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-11-29 23:54:24 +0700

    на проверку

---
+++ labs/5laba/5laba/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"input.txt");
+
+            var Regexes = new Dictionary<string, Regex>();
+            Regexes["«a»"] = new Regex(@"^a$");
+            Regexes["«aaaaaa»"] = new Regex(@"^a{6}$");
+            Regexes["«a aa a»"] = new Regex(@"^a aa a$");
+            Regexes["не менее 5 алфавитно-цифровых символов"] = new Regex(@"^[\w]{5,}$");
+            Regexes["email простого вида"] = new Regex(@"^[\w-]+@((\w+)|(\w[\w-]+))\.\w+$");
+            Regexes[" "] = new Regex(@"^(?:[гГ][гГ]\.\s+)?([а-яА-яёЁ]+(?:-[а-яА-ЯёЁ]+)?):?\s+(?:(\d+\.\d+)\s?){2}");
+
+            foreach (KeyValuePair<string, Regex> kvp in Regexes)
+            {
+                for (int i = 0; i < input.Length; ++i)
+                {
+                    //if (kvp.Value.IsMatch(input[i]))
+                    //    System.Console.WriteLine($"Регулярка {kvp.Key}: {input[i]}\nСтрока:{i + 1}");
+
+
+                }
+            }
+
+            //^(?:[гГ][гГ]\.\s+)?([а-яА-яёЁ]+(?:-[а-яА-ЯёЁ]+)?):?\s+(?:(\d+\.\d+)(\s*)){2}
+            var CityReg = new Regex(@"^(?:[гГ][гГ]\.\s+)?([а-яА-яёЁ]+(?:-[а-яА-ЯёЁ]+)?):?\s+(\d+\.\d+)\s+(\d+\.\d+)");   
+            var City = Console.ReadLine();
+            var Groups = CityReg.Split(City);
+            //int count = 0;
+            if (CityReg.IsMatch(City))
+            {
+                Console.WriteLine($"Город: {Groups[1]}\nШирота:{Groups[2]}\nДолгота:{Groups[3]}");
+
+            }
+            else
+            {
+                Console.WriteLine("Не найдено");
+            }
+            Console.WriteLine("=======================================");
+        }
+    }
+}
