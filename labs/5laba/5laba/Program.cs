71339a1300ff2963dbe5c03a9d07f191834d9d5e
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2022-12-17 17:21:49 +0700

    ..... правки

--- labs/5laba/5laba/Program.cs
+++ labs/5laba/5laba/Program.cs
@@ -11,24 +11,42 @@ namespace lab5
         {
             string[] input = File.ReadAllLines(@"input.txt");
 
-            var Regexes = new Dictionary<string, Regex>();
+            var Regexes = new Dictionary<string, Regex>(); // Dictionary<K, V> типизируется двумя типами:
+                                                           // параметр K представляет тип ключей, а параметр V предоставляет тип значений.
             Regexes["«a»"] = new Regex(@"^a$");
             Regexes["«aaaaaa»"] = new Regex(@"^a{6}$");
             Regexes["«a aa a»"] = new Regex(@"^a aa a$");
             Regexes["не менее 5 алфавитно-цифровых символов"] = new Regex(@"^[\w]{5,}$");
             Regexes["email простого вида"] = new Regex(@"^[\w-]+@((\w+)|(\w[\w-]+))\.\w+$");
             Regexes[" "] = new Regex(@"^(?:[гГ][гГ]\.\s+)?([а-яА-яёЁ]+(?:-[а-яА-ЯёЁ]+)?):?\s+(?:(\d+\.\d+)\s?){2}");
+            Regexes["fio"] = new Regex(@"^([А-ЯЁ][а-яё]+)\s+([А-ЯЁ][а-яё]+)(\s+[А-ЯЁ][а-яё]+)?\s?$"); // ФИО 
 
-            foreach (KeyValuePair<string, Regex> kvp in Regexes)
+             
+
+            //foreach (KeyValuePair<string, Regex> kvp in Regexes)
+            //{
+            //    for (int i = 0; i < input.Length; ++i)
+            //    {
+            //        //if (kvp.Value.IsMatch(input[i]))
+            //        //    System.Console.WriteLine($"Регулярка {kvp.Key}: {input[i]}\nСтрока:{i + 1}");
+
+
+            //    }
+            //}
+
+            Console.WriteLine("=======================================");
+            Console.WriteLine("Проверка ФИО  по ключу:");
+            string search = "";
+             search = Console.ReadLine();
+            if (Regexes["fio"].IsMatch(search))
+           
             {
-                for (int i = 0; i < input.Length; ++i)
-                {
-                    //if (kvp.Value.IsMatch(input[i]))
-                    //    System.Console.WriteLine($"Регулярка {kvp.Key}: {input[i]}\nСтрока:{i + 1}");
-
-
-                }
+                var groups = Regexes["fio"].Split(search);
+                Console.WriteLine($"Имя: {groups[1]}\n Фамилия {groups[2]}\n Отчество {groups[3]}");
             }
+            else
+            Console.WriteLine("Не является неймом по формату ключа, имя пишется с большой Буквы ");
+            Console.WriteLine("=======================================");
 
             //^(?:[гГ][гГ]\.\s+)?([а-яА-яёЁ]+(?:-[а-яА-ЯёЁ]+)?):?\s+(?:(\d+\.\d+)(\s*)){2}
             var CityReg = new Regex(@"^(?:[гГ][гГ]\.\s+)?([а-яА-яёЁ]+(?:-[а-яА-ЯёЁ]+)?):?\s+(\d+\.\d+)\s+(\d+\.\d+)");   
