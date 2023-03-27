e2cfd94891e49e6ecd9bbc77c78ad9be6c2905f6
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2023-03-27 19:34:44 +0700

    ////
    /////

---
+++ studybaselaba1/studybaselaba1/DataModel/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studybaselaba1.DataModel
+{
+     public class Group
+    {
+        public int IdGroup { get; set; }
+        public string Number { get; set; }
+        public string EducationForm { get; set; }
+        public ICollection<Discipline> Disciplines { get; set; }
+
+       
+    }
+}
