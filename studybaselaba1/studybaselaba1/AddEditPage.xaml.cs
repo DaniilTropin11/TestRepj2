1f5377ad40c7a3ed49793651d5b529217289eb43
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2023-04-01 13:20:18 +0700

    валидация , в отдельном классе

--- studybaselaba1/studybaselaba1/AddEditPage.xaml.cs
+++ studybaselaba1/studybaselaba1/AddEditPage.xaml.cs
@@ -23,11 +23,12 @@ namespace studybaselaba1
     /// </summary>
     public partial class AddEditPage : Page
     {
-       
+        Ocenka ocenka;
         public AddEditPage()
         {
             InitializeComponent();
-            //DataContext = _currentGroup;
+            ocenka = new Ocenka();
+            DataContext = ocenka;
             ComboNameGroup.ItemsSource = StudyContext.GetContext().Groups.ToList();
             ComboNameDiscipline.ItemsSource = StudyContext.GetContext().Disciplines.ToList();
             
@@ -52,43 +53,58 @@ namespace studybaselaba1
                 MessageBox.Show("Выберите группу", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             
-            string pattern5 = "^[0-9]+$|^-$";
-            bool isMatch = Regex.IsMatch(Text5.Text, pattern5);
+            //string pattern5 = "^[0-9]+$|^-$";
+            //bool isMatch = Regex.IsMatch(Text5.Text, pattern5);
 
-            if (!Regex.IsMatch(Text5.Text, pattern5))
+            //if (!Regex.IsMatch(Text5.Text, pattern5))
+            //{
+            //    errors.AppendLine("Строка содержит некорректные символы");
+            //    MessageBox.Show("Строка содержит некорректные символы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+
+
+            //string pattern4 = "^[0-9]+$|^-$";
+            //bool isMatch2 = Regex.IsMatch(Text4.Text, pattern4);
+
+            //if (!Regex.IsMatch(Text4.Text, pattern4))
+            //{
+            //    errors.AppendLine("Строка содержит некорректные символы");
+            //    MessageBox.Show("Строка содержит некорректные символы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+
+            //string pattern3 = "^[0-9]+$|^-$";
+            //bool isMatch3 = Regex.IsMatch(Text3.Text, pattern3);
+
+            //if (!Regex.IsMatch(Text3.Text, pattern3))
+            //{
+            //    errors.AppendLine("Строка содержит некорректные символы");
+            //    MessageBox.Show("Строка содержит некорректные символы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+
+            //string pattern2 = "^[0-9]+$|^-$";
+            //bool isMatch4 = Regex.IsMatch(Text2.Text, pattern2);
+
+            //if (!Regex.IsMatch(Text2.Text, pattern2))
+            //{
+            //    errors.AppendLine("Строка содержит некорректные символы");
+            //    MessageBox.Show("Строка содержит некорректные символы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+
+        }
+
+        private void Grid_Error(object sender, ValidationErrorEventArgs e)
+        {
+           
             {
-                errors.AppendLine("Строка содержит некорректные символы");
-                MessageBox.Show("Строка содержит некорректные символы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                if (e.Action == ValidationErrorEventAction.Added)
+                {
+                    (e.Source as TextBox).ToolTip = e.Error.ErrorContent.ToString();
+                }
+                else if (!((BindingExpressionBase)e.Error.BindingInError).HasError)
+                {
+                    (e.Source as TextBox).ToolTip = null;
+                }
             }
-
-
-            string pattern4 = "^[0-9]+$|^-$";
-            bool isMatch2 = Regex.IsMatch(Text4.Text, pattern4);
-
-            if (!Regex.IsMatch(Text4.Text, pattern4))
-            {
-                errors.AppendLine("Строка содержит некорректные символы");
-                MessageBox.Show("Строка содержит некорректные символы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            string pattern3 = "^[0-9]+$|^-$";
-            bool isMatch3 = Regex.IsMatch(Text3.Text, pattern3);
-
-            if (!Regex.IsMatch(Text3.Text, pattern3))
-            {
-                errors.AppendLine("Строка содержит некорректные символы");
-                MessageBox.Show("Строка содержит некорректные символы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            string pattern2 = "^[0-9]+$|^-$";
-            bool isMatch4 = Regex.IsMatch(Text2.Text, pattern2);
-
-            if (!Regex.IsMatch(Text2.Text, pattern2))
-            {
-                errors.AppendLine("Строка содержит некорректные символы");
-                MessageBox.Show("Строка содержит некорректные символы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
         }
     }
 }
---
+++ studybaselaba1/studybaselaba1/ValidationRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace studybaselaba1
+{
+
+    class OcenkaValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) 
+        {
+            string str = (string)value;
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return new ValidationResult(false, "Строка не может быть пустой!");
+            }
+            else if (Regex.IsMatch(str, @"^[0-9]+$|^-$") == false) // ^[А-ЯЁ][а-яё]{2}\-\d{2}$ 
+            {
+                return new ValidationResult(false, "Строка содержит недопустимые символы!");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+
+
+}
